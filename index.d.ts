8110b943cab35cbaf7d124da79ffc0312f22bebf
Luy Tran <octopitus@gmail.com>
2019-04-08 11:32:11 +0800

    Add TypeScript defination

---
+++ index.d.ts
@@ -0,0 +1,36 @@
+import {ReactElement, Component} from 'react'
+import {Animated, PanResponderGestureState} from 'react-native'
+
+interface Props {
+  height?: number
+  animatedValue?: Animated.Value
+  draggableRange?: {
+    top: number
+    bottom: number
+  }
+  snappingPoints?: number[]
+  minimumVelocityThreshold?: number
+  minimumDistanceThreshold?: number
+  avoidKeyboard?: boolean
+  onBackButtonPress?: () => void
+  onDragStart?: (value: number, gestureState: PanResponderGestureState) => void
+  onDragEnd?: (value: number, gestureState: PanResponderGestureState) => void
+  onMomentumDragStart?: (value: number) => void
+  onMomentumDragEnd?: (value: number) => void
+  allowMomentum?: boolean
+  allowDragging?: boolean
+  showBackdrop?: boolean
+  backdropOpacity?: number
+  friction?: number
+  children?: ReactElement | ((dragHandlers: any) => ReactElement)
+}
+
+interface AnimationConfig {
+  toValue: number
+  velocity: number
+}
+
+export default class SlidingUpPanel extends Component<Props> {
+  show: (value: number | AnimationConfig) => void
+  hide: () => void
+}
