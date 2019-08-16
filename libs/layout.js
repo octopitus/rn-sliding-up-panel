a1f63c8e132663f1788c6eec1ff24cf5d07e9c05
Luy Tran <octopitus@gmail.com>
2019-08-16 17:39:33 +0800

    ! Generate docs

--- libs/layout.js
+++
@@ -1,9 +0,0 @@
-import {StatusBar, Dimensions} from 'react-native'
-
-export function visibleHeight() {
-  return Dimensions.get('window').height
-}
-
-export function statusBarHeight() {
-  return StatusBar.currentHeight || 0
-}
